1cd42a706376cc054a7735dcc16cff706331ce21
shaunsargent <ShaunM10@gmail.com>
2024-04-19 21:33:38 -0400

    Worked on CategoryDetailScreen

--- app/src/main/java/com/example/androidrecipeapp/CategoryDetailScreen.kt
+++ app/src/main/java/com/example/androidrecipeapp/CategoryDetailScreen.kt
@@ -1,16 +1,38 @@
 package com.example.androidrecipeapp
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun CategoryDetailScreen(category: Category){
- Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-    Text("coming soon....")
- }
+fun CategoryDetailScreen(category: Category) {
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(category.strCategory, textAlign = TextAlign.Center)
+        Image(
+            // 3rd party lib that will load the image async
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = "${category.strCategory} Thumbnail",
+            modifier = Modifier
+                .wrapContentSize()
+                .aspectRatio(1f)
+        )
+        Text(text = category.strCategoryDescription, textAlign = TextAlign.Justify)
+
+    }
 }
