e397a73011a920a390ffe8cf1fe3fe1dfcbf6cee
Shaunm10 <shaunm10@gmail.com>
2024-04-22 17:29:45 -0400

    worked more on Navigation

--- app/src/main/java/com/example/androidrecipeapp/RecipeApp.kt
+++ app/src/main/java/com/example/androidrecipeapp/RecipeApp.kt
@@ -3,31 +3,39 @@ package com.example.androidrecipeapp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-
-import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
 fun RecipeApp(navController: NavHostController) {
-    val recipeViewModel : MainViewModel = viewModel()
+    val recipeViewModel: MainViewModel = viewModel()
     val viewState by recipeViewModel.categoriesState
 
 
-    NavHost(navController = navController, startDestination = Screen.RecipeScreen.route){
+    NavHost(navController = navController, startDestination = Screen.RecipeScreen.route) {
 
-        composable(route = Screen.RecipeScreen.route){
+        composable(route = Screen.RecipeScreen.route) {
 
-            RecipeScreen( viewState = viewState, navigateToDetail ={
+            RecipeScreen(viewState = viewState, navigateToDetail = {
                 // set the category in the back stack so next screen can get it.
                 navController.currentBackStackEntry?.savedStateHandle?.set("cat", it)
 
                 // finally use navigate to move to that screen.
                 navController.navigate(Screen.DetailScreen.route)
-            } )
+            })
 
         }
 
-        composable(route = Screen.DetailScreen.route)
+        composable(route = Screen.DetailScreen.route) {
+
+            // retrieve the categroy from the state
+            val category =
+                navController.previousBackStackEntry?.savedStateHandle?.get<Category>("cat")
+                    ?: Category("", "", "", "")
+
+            // and show this screen/component
+            CategoryDetailScreen(category = category)
+        }
     }
 }
